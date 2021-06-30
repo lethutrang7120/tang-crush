e45f54f96fe25442859476bbb0476486d1af044e
lethutrang7120 <86719447+lethutrang7120@users.noreply.github.com>
2021-06-30 18:10:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Lộc à!',
+    introTitle: 'Hội à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao cậu cứ đi nhầm đường
-    Lạc vào tim tớ lẻ loi
+    Mà sao mày cứ đi nhầm đường
+    Lạc vào tim tao lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng cậu đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng cậu đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích cậu <33',
-    btnNo: 'Không nha bạn iuuuu :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao Lộc lại thích tớ :3',
+    title: 'Phải chăng mày đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng mày đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích mày <33',
+    btnNo: 'Tát chết cụ mày bây giờ :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao mày lại chơi với taooo :3',
     btnReply: 'Gửi cho Trang <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Tớ biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
-    messDesc: 'Tớ xin lỗi cậu vì tất cả, cảm ơn cậu nhiều nhaaaaa :3.',
+    reply: 'Chơi thì chơi mà không chơi thì chơi <33333333',
+    mess: 'Tao biết mà 🥰. Thật là cảm động thứ tình bạn đẹp 😘😘',
+    messDesc: 'Yêu Hội nhất trên đời :3.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
